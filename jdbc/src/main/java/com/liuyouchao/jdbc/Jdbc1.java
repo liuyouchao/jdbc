7da7b4e912a8b029c77a31f00ce0cade2eaf2aa8
liuyouchao <liuyouchao@daojia.com>
2018-03-24 16:56:11 +0800

    提交

---
+++ jdbc/src/main/java/com/liuyouchao/jdbc/Jdbc1.java
@@ -0,0 +1,18 @@
+package com.liuyouchao.jdbc;
+
+/**
+ * jdbc 的基础连接数据库
+ * @author Administrator
+ *
+ */
+public class Jdbc1 
+{
+	
+	static void test() {
+
+	}
+    public static void main( String[] args )
+    {
+      
+    }
+}
