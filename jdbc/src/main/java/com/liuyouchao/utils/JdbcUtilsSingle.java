5ed59e90b237c0169ac314c7b1cbd2719efc33f6
liuyouchao <liuyouchao@daojia.com>
2018-04-06 19:02:59 +0800

    提交

--- jdbc/src/main/java/com/liuyouchao/utils/JdbcUtilsSingle.java
+++ jdbc/src/main/java/com/liuyouchao/utils/JdbcUtilsSingle.java
@@ -7,20 +7,35 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 /**
- * 静态方法
+ * 单例模式实现
  * 连接数据库的工具类
  * @author Administrator
  *
  */
-public final class JdbcUtils {
+public final class JdbcUtilsSingle {
 
-	//静态资源
-	private static String url = "jdbc:mysql://localhost:3306/jdbc";
-	private static String userName = "root";
-	private static String passWord = "root";
+	private  String url = "jdbc:mysql://localhost:3306/jdbc";
+	private  String userName = "root";
+	private  String passWord = "root";
+	//初始化加载，饿汉模式
+	//private  static JdbcUtilsSingle JdbcUtilsSimgle = new JdbcUtilsSingle();
+	//延迟加载，懒汉模式
+	private static JdbcUtilsSingle instant = null;
 	
-	private JdbcUtils() {}
+	private JdbcUtilsSingle() {}
 	
+	//提供对外获取单个对象的方法
+	public static JdbcUtilsSingle getJdbcUtil() {
+		//return JdbcUtilsSimgle;
+		if(null == instant) {
+			synchronized(JdbcUtilsSingle.class) {
+				if(null == instant) {
+					instant = new JdbcUtilsSingle(); 
+				}
+			}
+		}
+		return instant;
+	}
 	//静态代码块在类加载到虚拟机的时候执行，并且只执行一次
 	//注册只需要注册一次即可
 	static void registerDriver() throws Exception {
@@ -28,12 +43,12 @@ public final class JdbcUtils {
 	}
 	
 	//获取连接
-	public static Connection getConnection() throws Exception {	
+	public  Connection getConnection() throws Exception {	
 		return DriverManager.getConnection(url,userName,passWord);
 	}
 	
 	//释放资源
-	public static void free(ResultSet re,Statement st,Connection con) {
+	public  void free(ResultSet re,Statement st,Connection con) {
 		if(null != re) {
 			try {
 				re.close();
