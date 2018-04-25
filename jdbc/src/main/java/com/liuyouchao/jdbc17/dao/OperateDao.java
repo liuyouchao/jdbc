45c3f7b16a7ed9221172bd3734582e842f44f5a2
liuyouchao <liuyouchao@daojia.com>
2018-04-25 22:20:22 +0800

    提交

---
+++ jdbc/src/main/java/com/liuyouchao/jdbc17/dao/OperateDao.java
@@ -0,0 +1,17 @@
+package com.liuyouchao.jdbc17.dao;
+
+/**
+ * 三层结构中dao层的接口
+ * @author Administrator
+ *
+ */
+public interface OperateDao {
+
+	void create();
+	
+	void delete();
+	
+	void update();
+	
+	void read();
+}
---
+++ jdbc/src/main/java/com/liuyouchao/jdbc17/dao/impl/OperateDaoImpl.java
@@ -0,0 +1,47 @@
+package com.liuyouchao.jdbc17.dao.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.liuyouchao.jdbc17.dao.OperateDao;
+import com.liuyouchao.utils.JdbcUtils;
+
+/**
+ * dao层的实现类
+ * @author Administrator
+ *
+ */
+public class OperateDaoImpl implements OperateDao{
+
+	@Override
+	public void create() {
+		Connection  con = null;
+		PreparedStatement ps = null;
+		ResultSet re = null;
+		
+		
+		
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void read() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
